88ab09f5ce6f6b0ce45839f6b6e02dfbaa89887d
vyvuluan <vyvuluan@gmail.com>
2023-10-16 18:31:24 +0700

    UI combo, UI Home

--- Assets/Scripts/GameView.cs
+++ Assets/Scripts/GameView.cs
@@ -1,5 +1,6 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameView : MonoBehaviour
 {
@@ -9,9 +10,17 @@ public class GameView : MonoBehaviour
     [SerializeField] private TextMeshProUGUI countDownText;
     [SerializeField] private TextMeshProUGUI levelText;
 
+    [Header("COMBO")]
+    [SerializeField] private Image fillAmountCombo;
+    [SerializeField] private GameObject combo;
+    [SerializeField] private TextMeshProUGUI comboText;
+
     public void SetCountDownText(int minute, int second) => countDownText.text = $"{minute}:{second}";
     public void SetLevelText(string text) => levelText.text = text;
     public void SetStatusAcitvePopUpWin(bool status) => popUpWin.SetActive(status);
     public void SetStatusAcitvePopUpLose(bool status) => popUpLose.SetActive(status);
     public void SetStatusAcitveAddSlot(bool status) => addSlot.SetActive(status);
+    public void SetStatusActiveCombo(bool status) => combo.SetActive(status);
+    public void SetFillAmountCombo(float amount) => fillAmountCombo.fillAmount = amount;
+    public void SetComboText(string text) => comboText.text = text;
 }
